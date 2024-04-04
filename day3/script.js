3d34f265c81c48dfc5d1492298eb1bb8a8b432f8
Jigyasa0411 <147132701+Jigyasa0411@users.noreply.github.com>
2024-04-04 15:03:51 +0530

    parse Int in js

--- day3/script.js
+++ day3/script.js
@@ -10,41 +10,61 @@
 //console.log(message);
  
 
-function enc(){
-    console.log(message);
-    var message ="hello";
-    console .log(message);
-    var message= "else";
-    console.log(message);
-}
+// function enc(){
 
-enc
+//     console.log(message);
+//     var message ="hello";
+//     console .log(message);
+//     var message= "else";
+//     console.log(message);
+// }
 
-function env(){
-    for(var i=0;i<5;i++){
-        console.log('---',i);
-    }
-    console.log(i);
-}
+// enc
+
+// function env(){
+//     for(var i=0;i<5;i++){
+//         console.log('---',i);
+//     }
+//     console.log(i);
+// }
 
 
-env  
+// env  
 
 //gobally intoduction
 //
-let message="hello";
-message="else";
-console.log(message);
+// let message="hello";
+// message="else";
+// console.log(message);
 
-let userAge="abcs";
-userAge="abcs";
-console.log(typeof(userAge));
-let arr=['a','b','c','d'];
-arr[0]='abcd';
-console.log(arr);
+// let userAge="abcs";
+// userAge="abcs";
+// console.log(typeof(userAge));
+// let arr=['a','b','c','d'];
+// arr[0]='abcd';
+// console.log(arr);
 
 
-let str="12";
-let age=20;
-let ans=age+str;
-console.log(ans);
+// let str="12";
+// let age=20;
+
+// let ans=(age+(str*1));
+// console.log(ans);
+
+
+//parse Int
+// let str='12a';
+// let p=parseInt(str);
+// let n=number(str);
+// console.log(p,n);
+
+let a='12';
+let b=12;
+if(a==b){
+    console.log('yes');
+}
+else{
+    console.log('no');
+
+}
+
