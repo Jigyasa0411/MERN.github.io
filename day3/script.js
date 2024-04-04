623a5678ed08c501ca21da9d6a9497d58e2ceb02
Jigyasa0411 <147132701+Jigyasa0411@users.noreply.github.com>
2024-04-04 14:18:26 +0530

    introduction of javascript

---
+++ day3/script.js
@@ -0,0 +1,37 @@
+//var message='Hello world';
+//var age=10;
+//age="done";//loosely typed
+
+//hoisting
+//console.log(message);
+//var message="hello";
+//console.log(message);
+//message="else";
+//console.log(message);
+ 
+
+function enc(){
+    console.log(message);
+    var message ="hello";
+    console .log(message);
+    var message= "else";
+    console.log(message);
+}
+
+enc
+
+function env(){
+    for(var i=0;i<5;i++){
+        console.log('---',i);
+    }
+    console.log(i);
+}
+
+
+env  
+
+//gobally intoduction
+//
+let message="hello";
+message="else";
+console.log(message);
