f9086459205e97e32c1adbc315716cb7a9184eb7
Jigyasa0411 <147132701+Jigyasa0411@users.noreply.github.com>
2024-04-05 14:04:09 +0530

    remove second

--- day 4/script3.js
+++ day 4/script3.js
@@ -3,10 +3,7 @@ document.querySelector('div');
 // console.log(div);
 // div.innerHTML="<p>hello</p>"
 // div[0].innerHTML="<p>hello</p>"
-const c=document.querySelector('p');
-
-
-
+const c=document.querySelector('p:nth-of-type(2)');
 console.log(res);
 
 res.removeChild(c);
