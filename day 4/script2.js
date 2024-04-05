c056e8eebbc0b62bd71d1da00c7b103f16a5df5e
Jigyasa0411 <147132701+Jigyasa0411@users.noreply.github.com>
2024-04-05 10:33:46 +0530

    program to check whether it is object or not

--- day 4/script2.js
+++ day 4/script2.js
@@ -8,20 +8,40 @@
 //     console.log("no");
 // }
 
-const obj={
-    "name": "Ajay",
-    "lastname":"singh",
+// const obj={
+//     "name": "Ajay",
+//     "lastname":"singh",
+// }
+// // // const str=json.stringify(obj);
+// // // // console.log(obj);
+// // // // document.write(obj);
+
+// // obj.name=20;
+// // console.log(obj);
+
+// const arr=[1,2,3,4];
+// arr[10]=20;
+// console.log(Array.isArray(arr));
+// console.log(Array.isArray(obj));
+
+//program to find if it is object or not
+function checkIfObject(variable) {
+    if (typeof variable === 'object') {
+        return 'yes';
+    } else {
+        return 'no';
+    }
 }
-// // const str=json.stringify(obj);
-// // // console.log(obj);
-// // // document.write(obj);
 
-// obj.name=20;
-// console.log(obj);
 
-const arr=[1,2,3,4];
-arr[10]=20;
-console.log(Array.isArray(arr));
-console.log(Array.isArray(obj));
+const variable1 = { a: 1, b: 2 };
+const variable2 = [1, 2, 3];
+const variable3 = 'Hello';
+console.log(typeof(variable3));
+
+console.log(checkIfObject(variable1)); 
+console.log(checkIfObject(variable2)); 
+console.log(checkIfObject(variable3)); 
+
 
 
