dd99248148247f655a3faed18b1a6426ab56bfad
Jigyasa0411 <147132701+Jigyasa0411@users.noreply.github.com>
2024-04-05 10:10:53 +0530

    initialising an array

--- day 4/script2.js
+++ day 4/script2.js
@@ -8,15 +8,19 @@
 //     console.log("no");
 // }
 
-const obj={
-    "name": "Ajay",
-    "lastname":"singh",
-}
-// const str=json.stringify(obj);
-// // console.log(obj);
-// // document.write(obj);
+// const obj={
+//     "name": "Ajay",
+//     "lastname":"singh",
+// }
+// // const str=json.stringify(obj);
+// // // console.log(obj);
+// // // document.write(obj);
 
-obj.name=20;
-console.log(obj);
+// obj.name=20;
+// console.log(obj);
+
+let arr=[1,2,3,4];
+arr=[2,3,4];
+console.log(arr);
 
 
