79b7528d129f05d9d9a2e863562d5f43e58990c2
Jigyasa0411 <147132701+Jigyasa0411@users.noreply.github.com>
2024-04-05 10:46:35 +0530

    for of loop

--- day 4/script2.js
+++ day 4/script2.js
@@ -25,23 +25,35 @@
 // console.log(Array.isArray(obj));
 
 //program to find if it is object or not
-function checkIfObject(variable) {
-    if (typeof variable === 'object' && variable !== null && !Array.isArray(variable)) {
-        return 'yes';
-    } else {
-        return 'no';
-    }
+// function checkIfObject(variable) {
+//     if (typeof variable === 'object' && variable !== null && !Array.isArray(variable)) {
+//         return 'yes';
+//     } else {
+//         return 'no';
+//     }
+// }
+
+
+// const variable1 = { a: 1, b: 2 };
+// const variable2 = [1, 2, 3];
+// const variable3 = 'Hello';
+// console.log(typeof(variable3));
+
+// console.log(checkIfObject(variable1)); 
+// console.log(checkIfObject(variable2)); 
+// console.log(checkIfObject(variable3)); 
+ const arr=['name',2,true];
+ const obj={
+    'name':'Ajay' , age:20 ,1:'else'
+ }
+// for(let i=0;i<4;i++){
+//     console.log(i);
+//}
+// 
+for(let i of arr){
+    console.log(i);
 }
-
-
-const variable1 = { a: 1, b: 2 };
-const variable2 = [1, 2, 3];
-const variable3 = 'Hello';
-console.log(typeof(variable3));
-
-console.log(checkIfObject(variable1)); 
-console.log(checkIfObject(variable2)); 
-console.log(checkIfObject(variable3)); 
-
-
+for(let i of obj){
+    console.log(i);
+}
 
