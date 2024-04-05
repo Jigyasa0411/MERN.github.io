e918db6f31a4dc1ef45c11ac64c117a31bee01bd
Jigyasa0411 <147132701+Jigyasa0411@users.noreply.github.com>
2024-04-05 14:25:16 +0530

    created button

--- day 4/script3.js
+++ day 4/script3.js
@@ -1,9 +1,17 @@
-const res=
-document.querySelector('div');
-// console.log(div);
-// div.innerHTML="<p>hello</p>"
-// div[0].innerHTML="<p>hello</p>"
-const c=document.querySelector('p:nth-of-type(2)');
-console.log(res);
+// const res=
+// document.querySelector('div');
+// // console.log(div);
+// // div.innerHTML="<p>hello</p>"
+// // div[0].innerHTML="<p>hello</p>"
+// const c=document.querySelector('p:nth-of-type(2)');
+// console.log(res);
 
-res.removeChild(c);
+// res.removeChild(c);
+function getInfo(){
+//     const d=document.querySelector("div");
+//    // const el=document.createElement("");
+//     el.innerText="btn was clicked";
+    // d.prepend("my name is jigyasa");
+    console.log("Btn clicked");
+    document.body.innerHTML="this button was clicked";
+}
