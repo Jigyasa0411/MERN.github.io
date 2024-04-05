73b12680027af9a15c3cffbf6f3813f66cf4e0ef
Jigyasa0411 <147132701+Jigyasa0411@users.noreply.github.com>
2024-04-05 16:09:07 +0530

    modified form

--- day 4/script4.js
+++ day 4/script4.js
@@ -13,12 +13,15 @@ function submitForm(e){
     e.preventDefault();
     //console.dir(e.target.value);
     const t= e.target
+    const res={};
     for(let i=0;i<t.length;i++){
     //    console.dir(t[i].value);
     //    console.log(t[i].type);
        const ty=t[i].type;
        const vl=t[i].value;
-       console.log(ty,vl);
+       const nm=t[i].name;
+       console.log(nm,ty,vl);
+       res[nm]=vl;
        if(ty=='checkbox'){
         console.log(t[i].checked)
        }
