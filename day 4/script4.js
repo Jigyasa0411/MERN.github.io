18168c3b5e1e32363510bd531862e77fe025be67
Jigyasa0411 <147132701+Jigyasa0411@users.noreply.github.com>
2024-04-05 15:30:00 +0530

    add css in form

--- day 4/script4.js
+++ day 4/script4.js
@@ -1,7 +1,15 @@
-const firstNameChange=(e)=>{
-    // const val=e.target.value;
-    //  if(val.length>3){
-    //     console.log('correct');
-    //  }
-    console.log(e);
+// const firstNameChange=(e)=>{
+//     // const val=e.target.value;
+//     //  if(val.length>3){
+//     //     console.log('correct');
+//     //  }
+//     console.log(e);
+// }
+const firstNameChange=()=>{
+    console.log("firstname");
+
+}
+function submitForm(e){
+    e.preventDefault();
+    console.log(e)
 }
