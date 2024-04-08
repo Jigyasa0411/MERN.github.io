886bdb3f686fed6a774f0d72ab41853e0975ce15
Jigyasa0411 <147132701+Jigyasa0411@users.noreply.github.com>
2024-04-08 14:49:44 +0530

    fetching api

--- day5/script5.js
+++ day5/script5.js
@@ -30,15 +30,39 @@
 // // console.log('GEC End');
 // const btn=document.getElementById('btn');
 // btn.addEventListener("click",printpretty);
-console.log('start')
-// const req =fetch("https://api.github.com/users/vikhyat9690");
+// console.log('start')
+// // const req =fetch("https://api.github.com/users/vikhyat9690");
 
-// console.log(req);
-// req.then(()=>console.log('Fetched'));
+// // console.log(req);
+// // req.then(()=>console.log('Fetched'));
 
+// // console.log('End');
+//  const  req=fetch('https://dummyjson.com/products/1')
+// const res=req.then(res=> res.json())
+// res.then((data)=>console.log(data));
+// //.then(res => console.log(res))
+// // .then(json => console.log(json))
 // console.log('End');
-fetch('https://dummyjson.com/products/1')
-.then(res => console.log(res))
-// .then(json => console.log(json))
-console.log('End');
-            
+console.log("hello");
+function callApi(){
+ fetch("https://newsapi.org/v2/top-headlines?country=in&apikey=00f57a85abee4418ac93ddd066f4bbf0")
+ .then(res=>res.json())
+ .then((res)=>{
+    console.log(res);
+ })
+}
+//   //callApi();
+  
+  function renderUI(data){
+    const articles= data.articles;
+    
+   const root = document.getElementById("root");
+   const ar=articles[0];
+   
+   console.log(ar);
+    const div=document.createElement("div");
+    div.innerText='card';
+
+    root.appendChild(div);
+  }
+  callApi();
