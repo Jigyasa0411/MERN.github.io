87594114ab888791daa810bad22bad921d9332e9
Jigyasa0411 <147132701+Jigyasa0411@users.noreply.github.com>
2024-04-08 11:26:35 +0530

    set timeout function

--- day5/script5.js
+++ day5/script5.js
@@ -1,17 +1,24 @@
-let arr=[2,10,21,32];
- function printPretty(elem){
-    console.log(':',elem);
- }
+// let arr=[2,10,21,32];
+//  function printPretty(elem){
+//     console.log(':',elem);
+//  }
 
-function printarray(arr){
-    for(let i=0;i<arguments.length;i++){
-        console.log ( arr[i]);
+// function printarray(arr){
+//     for(let i=0;i<arguments.length;i++){
+//         console.log ( arr[i]);
 
-    }
-    arr.forEach( (a,b,c)=>{
-        console.log(':',a,b,c);
-     });
+//     }
+//     arr.forEach( (a,b,c)=>{
+//         console.log(':',a,b,c);
+//      });
+
+
         
     
+// }
+// printarray(arr);
+ function printpretty(elem){
+ console.log(':',elem);
 }
-printarray(arr);
+
+setTimeout(printpretty, 10000);
