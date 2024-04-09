a7183a96a232c231418e285be5c40f99c0e0fe89
Jigyasa0411 <147132701+Jigyasa0411@users.noreply.github.com>
2024-04-09 11:31:29 +0530

    promises

--- day6/script6.js
+++ day6/script6.js
@@ -13,15 +13,15 @@
 //     console.log(orderID)
 // }
 // createOrder('soap',makePayment;)
-const pr=new Promise((resolve,reject)=>{
-    setTimeout(() => {reject("some items are out of stock"):}0);
+// const pr=new Promise((resolve,reject)=>{
+//     setTimeout(() => {reject("some items are out of stock"):}0);
         
-    });
-    console.log(pr)
-    pr.then((res)=>{
-        console.log(res);
-    }).catch((err)=>{console.log(err);})
-//     if(id=='ise123'){
+//     });
+//     console.log(pr)
+//     pr.then((res)=>{
+//         console.log(res);
+//     }).catch((err)=>{console.log(err);})
+// //     if(id=='ise123'){
 //        resolve();
 // } Math.random()%1
 //   else{
@@ -29,3 +29,12 @@ const pr=new Promise((resolve,reject)=>{
 // }   
 // });
 // console.log(pr);
+setTimeout(function abc(){ console.log('I am one');},0);
+const pr= new Promise((res,rej)=>{setTimeout(()=>{res('done')},0);})
+
+pr.then(function b(res){
+    console.log("promise completed",res);
+});
+setTimeout(function xyz(){
+    console.log('I am top');
+})
