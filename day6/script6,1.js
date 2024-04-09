fca7457b8551501f15458c24d1ee61b057a1e9ba
Jigyasa0411 <147132701+Jigyasa0411@users.noreply.github.com>
2024-04-09 11:45:28 +0530

    map function

--- day6/script6,1.js
+++ day6/script6,1.js
@@ -1,11 +1,5 @@
-console.log('hello');
+// 
 const arr=[10,22,34];
-
-const ans=arr.map((a)=>{
-    console.log(a);
-    return a*2;
-});
-const ans2=arr.map(a=>Math.pow(a,2));
-
-console.log(ans);
-console.log(ans2);
+const arr2=[10.11,22.32,34.76];
+const ans3=arr.map(a=>parseInt(a));
+console.log(ans3);
