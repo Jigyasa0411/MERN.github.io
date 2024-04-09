383d982138794663c863ceed2e231ad314c2d441
Jigyasa0411 <147132701+Jigyasa0411@users.noreply.github.com>
2024-04-09 15:39:38 +0530

    fetch api

--- nodejs/node2.js
+++ nodejs/node2.js
@@ -2,13 +2,18 @@
 // figlet("jigyasa",(err,data)=>{
 //     console.log(data);
 // })
-fetch('https://api.github.com/users/deepak3440').then((res)=>{
-     return res.json();
+// fetch('https://api.github.com/users/deepak3440').then((res)=>{
+//      return res.json();
 
-})
-.then((data)=>{
-    console.log(data);
-})
-.catch((err)=>{
-    console.log('Erroe occurred!')
-})
+// })
+// .then((data)=>{
+//     console.log(data);
+// })
+// .catch((err)=>{
+//     console.log('Erroe occurred!')
+// })
+function getApi(){
+    const pr=fetch('https://api.github.com/users/deepal3440')
+    console.log(pr);
+}
+ getApi();
