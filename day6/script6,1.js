764d007511df15c973374aaf29433046455a2e25
Jigyasa0411 <147132701+Jigyasa0411@users.noreply.github.com>
2024-04-09 11:54:42 +0530

    array

--- day6/script6,1.js
+++ day6/script6,1.js
@@ -1,5 +1,18 @@
 // 
+// const arr=[10,22,34];
+// const arr2=[10.11,22.32,34.76];
+// const ans3=arr.map(a=>parseInt(a));
+// console.log(ans3);
+// const arr=[10,22,34];
+// const ans=arr.push(99);
+// const arr2=arr.map((a)=>a*2);
+// console.log(ans);
+// console.log(arr1);
+// console.log(arr2)
 const arr=[10,22,34];
-const arr2=[10.11,22.32,34.76];
-const ans3=arr.map(a=>parseInt(a));
-console.log(ans3);
+const ans=arr.filter((a)=>{
+    if(a>20 )return true;
+    else return false;
+})
+console.log(arr);
+console.log(ans);
