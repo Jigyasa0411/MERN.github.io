b821adb598e78bef7caea67e007a218b1fd031aa
Jigyasa0411 <147132701+Jigyasa0411@users.noreply.github.com>
2024-04-10 12:06:05 +0530

    creating local host

--- day7/internal_module/buffer.js
+++ day7/internal_module/buffer.js
@@ -1,4 +1,33 @@
-const b=new Buffer.from('abc');
-console.log(b.toString());
-b.write('other');
-console.log(b.toString());
+// const b=new Buffer.from('abc');
+// console.log(b.toString());
+// b.write('other');
+// console.log(b.toString());
+// // const fs= require('fs');
+// fs.writeFileSync('./myReadme.txt',"10th April 2024:Day7");
+// const fs=require('fs');
+// //const { isPromise } = require('util/types');
+// // console.log("start");
+// // const data=fs.readFileSync('.myReadme.txt',{encoding:'utf8'/});
+// // console.log(data);
+// // console.log("end");
+// const pr=isPromise.readfild-('.myReadme.txt',{encoding:'utf8'});
+// pr.then((res)=>{
+//     console.log(res);
+// });
+//  console.log("end")
+
+
+// const fs=require('fs');
+// const data=fs.readFileSync('./myReadme.txt',{encoding: "UTF8"},(err,data)=>
+// //console.log(data)
+// );
+const http = require('http');
+const app= http.createServer((req,res)=>{
+    console.log('request recieved')
+    console.log(req.url);
+    res.end("hello");
+});
+
+app.listen(1400);
+
+
